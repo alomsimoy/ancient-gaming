0f7850987e1bc43c1da7b0dae148f25fe5565482
Alex Piqueras <alex@piqueras.me>
2021-02-03 22:25:57 +0100

    Cleanup sample code

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UserModule } from './user/user.module';
 import { BetModule } from './bet/bet.module';
@@ -10,7 +8,7 @@ import { BetModule } from './bet/bet.module';
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      typePaths: ['./**/*.graphql']
+      typePaths: ['./**/*.graphql'],
     }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
@@ -24,8 +22,5 @@ import { BetModule } from './bet/bet.module';
     UserModule,
     BetModule,
   ],
-  controllers: [AppController],
-  providers: [AppService]
 })
-
 export class AppModule {}
